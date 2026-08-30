c2777cdb03cb6a00f25d85b7ff8807bc19bbd6e1
contamediafire94-ai <contamediafire94@gmail.com>
2026-08-30 04:07:50 -0300

    MainActivity.java

--- app/src/main/java/com/samp/mobile/launcher/activity/MainActivity.java
+++ app/src/main/java/com/samp/mobile/launcher/activity/MainActivity.java
@@ -1,8 +1,13 @@
 package com.samp.mobile.launcher.activity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.samp.mobile.R;
+import com.samp.mobile.game.SAMP;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Button jogar = findViewById(R.id.button_play);
+
+        jogar.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SAMP.class);
+            startActivity(intent);
+        });
     }
 }
