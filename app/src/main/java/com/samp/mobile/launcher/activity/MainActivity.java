5fcb9eacdeb82f0fb784a06ea308026ff55069d6
contamediafire94-ai <contamediafire94@gmail.com>
2026-08-30 04:06:47 -0300

    MainActivity.java

--- app/src/main/java/com/samp/mobile/launcher/activity/MainActivity.java
+++ app/src/main/java/com/samp/mobile/launcher/activity/MainActivity.java
@@ -2,11 +2,14 @@ package com.samp.mobile.launcher.activity;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.samp.mobile.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
     }
 }
