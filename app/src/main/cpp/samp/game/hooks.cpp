d523cd9df26f96a82a8d1b75d07132abcd0cba59
contamediafire94-ai <contamediafire94@gmail.com>
2026-08-31 16:08:47 -0300

    hooks.cpp

--- app/src/main/cpp/samp/game/hooks.cpp
+++ app/src/main/cpp/samp/game/hooks.cpp
@@ -1945,8 +1945,8 @@ void InstallSpecialHooks()
 
     CHook::InlineHook("_Z10NvUtilInitv", &NvUtilInit_hook, &NvUtilInit);
 
-    CHook::RET("_ZN12CCutsceneMgr16LoadCutsceneDataEPKc"); // LoadCutsceneData
-    CHook::RET("_ZN12CCutsceneMgr10InitialiseEv");			// CCutsceneMgr::Initialise
+  //  CHook::RET("_ZN12CCutsceneMgr16LoadCutsceneDataEPKc"); // LoadCutsceneData
+  //  CHook::RET("_ZN12CCutsceneMgr10InitialiseEv");			// CCutsceneMgr::Initialise
 
     CHook::Redirect("_Z7NvFOpenPKcS0_bb", &NvFOpen);
 
